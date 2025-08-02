9c4cbf4c8179833808d538b1ed24e8a391eab767
Mohamed Nabil Mohamed Abusarea <mnbil0680@gmail.com>
2025-08-02 19:17:48 +0300

    Controllers

--- LinkifyPLL/Controllers/AccountController.cs
+++ LinkifyPLL/Controllers/AccountController.cs
@@ -6,22 +6,28 @@ namespace LinkifyPLL.Controllers
 {
     public class AccountController : Controller
     {
+        public readonly IUserService IUS;
         private readonly IAuthenticationService _authService;
-        public AccountController(IAuthenticationService authService)
+
+        // Constructor to initialize IUserService
+        public AccountController(IUserService ius, IAuthenticationService authService)
         {
-            _authService = authService;
+            this.IUS = ius;
+            this._authService = authService;
         }
-        public IActionResult Index()
-        {
-            return View();
+
+        public IActionResult Index(){
+            return RedirectToAction("Login");
         }
+
         [HttpGet]
-        public IActionResult Login() { 
+        public IActionResult Login()
+        {
             return View();
         }
+
         [HttpPost]
-        public async Task<IActionResult> Login(UserLoginMV model)
-        {
+        public async Task<IActionResult> Login(UserLoginMV model){
             var user = await _authService.LoginAsync(model.Email, model.Password, true);
             if (user == null)
             {
@@ -30,5 +36,32 @@ namespace LinkifyPLL.Controllers
             }
             return RedirectToAction("Index", "Home");
         }
+
+        [HttpGet]
+        public async Task<IActionResult> Signup()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Signup(UserRegisterMV model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var result = await IUS.RegisterUserAsync(model, model.Password);
+            if (!result)
+            {
+                ModelState.AddModelError("", "Signup failed. Please try again.");
+                return View(model);
+            }
+
+            return RedirectToAction("Login");
+        }
+
+       
     }
+
 }
