6efea9e07aed901109092af098aa35b47267bfd0
sohilagamall <sohilagamal30@gmail.com>
2025-08-03 09:24:02 +0300

    added the delete and get in repo

---
+++ LinkifyDAL/Repo/Abstraction/IContactRepository.cs
@@ -0,0 +1,16 @@
+﻿using LinkifyDAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkifyDAL.Repo.Abstraction
+{
+    public interface IContactRepository
+    {
+        public List<Contact> GetByUserId(string userId);
+        public Contact GetContactId(int id);
+        public void Delete(int id);
+    }
+}
---
+++ LinkifyDAL/Repo/Implementation/ContactRepository.cs
@@ -0,0 +1,50 @@
+﻿using LinkifyDAL.DataBase;
+using LinkifyDAL.Entities;
+using LinkifyDAL.Repo.Abstraction;
+
+
+namespace LinkifyDAL.Repo.Implementation
+{
+    public class ContactRepository : IContactRepository
+    {
+        private readonly LinkifyDbContext _context;
+        public ContactRepository(LinkifyDbContext context)
+        {
+            _context = context;
+        }
+        public List<Contact> GetByUserId(string userId)
+        {
+            if (string.IsNullOrEmpty(userId))
+            {
+                throw new ArgumentException("User ID cannot be null or empty", nameof(userId));
+            }
+            return _context.Contacts
+                .Where(c => c.UserId == userId && !c.MarkedAsDeleted())
+                .ToList();
+        }
+        public Contact GetContactId(int id)
+        {
+            if (id <= 0)
+            {
+                throw new ArgumentException("Invalid contact ID", nameof(id));
+            }
+            return _context.Contacts
+                .FirstOrDefault(c => c.Id == id);
+        }
+        public void Delete(int id)
+        {
+           if(id <= 0)
+            {
+                throw new ArgumentException("Invalid contact ID", nameof(id));
+            }
+            var contact = GetContactId(id);
+            if (contact == null)
+            {
+                throw new KeyNotFoundException($"Contact with ID {id} not found.");
+            }
+            contact.MarkedAsDeleted();
+            _context.SaveChanges();
+        }
+
+    }
+}
