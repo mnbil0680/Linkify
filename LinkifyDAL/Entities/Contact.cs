2d8e579de0842762c95186465f609b5bd7f779ea
sohilagamall <sohilagamal30@gmail.com>
2025-08-03 08:49:39 +0300

    added contact entity

---
+++ LinkifyDAL/Entities/Contact.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkifyDAL.Entities
+{
+    public class Contact
+    {
+        public int Id { get; private set; }
+        [Required]
+        public string Type { get; private set; } // e.g., "Email", "Phone", "Social Media"
+        [Required]
+        public string Value { get; private set; }
+        [Required]
+        public string UserId { get; set; } // FK to Identity User
+        public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+        public DateTime? ModifiedOn { get; set; }
+        public DateTime? DeletedOn { get; set; }
+    }
+}
