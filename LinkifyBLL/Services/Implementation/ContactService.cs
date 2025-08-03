e82e8b51abf67108e8a32ad5ac78c31ffffeae2c
sohilagamall <sohilagamal30@gmail.com>
2025-08-03 09:23:29 +0300

    added delete and get for the contacts

---
+++ LinkifyBLL/Services/Implementation/ContactService.cs
@@ -0,0 +1,40 @@
+﻿using LinkifyBLL.ModelView;
+using LinkifyBLL.Services.Abstraction;
+using LinkifyDAL.Repo.Abstraction;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkifyBLL.Services.Implementation
+{
+    public class ContactService : IContactService
+    {
+        private readonly IContactRepository _contactRepository;
+        private readonly UserManager<UserMV> _userManager;
+            public ContactService(IContactRepository contactRepository, UserManager<UserMV> userManager)
+        {
+            _contactRepository = contactRepository;
+            _userManager = userManager;
+        }
+        public List<ContactDTO> GetContactsByUserId(string userId)
+        {
+            var contacts = _contactRepository.GetByUserId(userId);
+            var contactDTOs = contacts.Select(c => new ContactDTO
+            {
+                Id = c.Id,
+                Type = c.Type,
+                Value = c.Value,
+            }).ToList();
+            return contactDTOs;
+        }
+        public void DeleteContact(int id)
+        {
+            _contactRepository.Delete(id);
+        }
+
+        
+    }
+}
