3e7ad154945864f6586d6316f26627dee3e1a6fb
Mohamed Nabil Mohamed Abusarea <mnbil0680@gmail.com>
2025-08-03 05:59:47 +0300

    DAL

---
+++ LinkifyDAL/Migrations/20250802234552_2nd_initDB.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LinkifyDAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class _2nd_initDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
