95c671ac1bb4c4bbeab572eb751bf8df9c0f0d43
Mohamed Nabil Mohamed Abusarea <mnbil0680@gmail.com>
2025-08-09 01:06:39 +0300

    Create HomeMV

---
+++ LinkifyBLL/ModelView/HomeMV.cs
@@ -0,0 +1,22 @@
+﻿using LinkifyDAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkifyBLL.ModelView
+{
+    public class HomeMV
+    {
+
+        //For Identity User
+
+        public List<PostImages> PostImages{get; set;}
+        public List<SharePost> SharePosts { get; set; }
+        public List<PostReactions> PostReactions { get; set; }
+        public List<PostComments> PostComments { get; set; }
+        public List<Post> Posts { get; set; }
+
+    }
+}
